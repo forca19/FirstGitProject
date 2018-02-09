166c3050dd05de01fa21428117c420a6ca941737
Fernando Ortiz <jfortiz19@gmail.com>
2018-02-08 22:12:53 -0600

    Adding a new String var just because

--- src/com/git/testing/TestingClass.java
+++ src/com/git/testing/TestingClass.java
@@ -3,7 +3,7 @@ package com.git.testing;
 public class TestingClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String yo = "I dont know what this would do";
 
 	}
 
